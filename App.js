35955341f743cca931bc2ced048cc33d4dae4e42
NaNut <galunyu@gmail.com>
2020-10-18 09:05:47 +0700

    [Learn] layout flexDirection

--- App.js
+++ App.js
@@ -1,21 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, TouchableNativeFeedback, Button, Alert, Platform } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row" }}>
+        <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}></View>
+        <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+        <View style={{ backgroundColor: "tomato", width: 100, height: 100 }}></View>
+
+      </View>
+      <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "row-reverse" }}>
+        <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}></View>
+        <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+        <View style={{ backgroundColor: "tomato", width: 100, height: 100 }}></View>
+
+      </View>
+      <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-between" }}>
+        <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "column" }}>
+          <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}></View>
+          <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+          <View style={{ backgroundColor: "tomato", width: 100, height: 100 }}></View>
+
+        </View>
+        <View style={{ backgroundColor: "#fff", flex: 1, flexDirection: "column-reverse" }}>
+          <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}></View>
+          <View style={{ backgroundColor: "gold", width: 100, height: 100 }}></View>
+          <View style={{ backgroundColor: "tomato", width: 100, height: 100 }}></View>
+
+        </View>
+
+      </View>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? 50 : 0
   },
 });
