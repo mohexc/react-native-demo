0210e54abf460ff2badf0ca001ae354d6f60a2ec
NaNut <galunyu@gmail.com>
2020-10-18 15:49:15 +0700

    [Added] ViewImageScreen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Platform } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   return (
-
-    <WelcomeScreen />
+    <ViewImageScreen />
+    // <WelcomeScreen />
   );
 }
 
---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Image, StyleSheet, View } from 'react-native'
+import color from "../configs/color"
+const ViewImageScreen = () => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.closeIcon}></View>
+            <View style={styles.deleteIcon}></View>
+            <Image
+                resizeMode="contain"
+                style={styles.image}
+                source={{ uri: "https://picsum.photos/500/600" }}
+            />
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    closeIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: color.primary,
+        position: "absolute",
+        top: 40,
+        left: 30
+    },
+    deleteIcon: {
+        width: 50,
+        height: 50,
+        backgroundColor: color.secondary,
+        position: "absolute",
+        top: 40,
+        right: 30
+    },
+    container: {
+        backgroundColor: color.black,
+        flex: 1,
+    },
+    image: {
+        width: "100%",
+        height: "100%"
+    }
+})
+
+export default ViewImageScreen
